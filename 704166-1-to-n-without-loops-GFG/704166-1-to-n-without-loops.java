884cf2486659fd30e3eebc2d3979aa526f264026
zainav7428 <zainav7428@gmail.com>
2026-07-29 22:20:53 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 704166-1-to-n-without-loops-GFG/704166-1-to-n-without-loops.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void printTillN(int n) {
+        // Base Case
+        if (n == 0) {
+            return;
+        }
+
+        // Recursive Call
+        printTillN(n - 1);
+
+        // Print after recursion
+        System.out.print(n + " ");
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
