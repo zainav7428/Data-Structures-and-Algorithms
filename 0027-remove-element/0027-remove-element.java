6fb35610a7ac43107f5ef42bddd49f02f135b5c7
zainav7428 <zainav7428@gmail.com>
2026-08-12 21:20:08 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.63%) - LeetHub - Time: O(n), Space: O(1)

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,19 +1,21 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k=0;
+        int j = 0;
+        int count = 0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
-                nums[k]=nums[i];
-                k++;
+
+
+int temp = nums[i];
+nums[i] = nums[j];
+nums[j] = temp;
+j++;
+count++;
             }
         }
-        return k;
+        return count;
     }
 }
-
-        
-    
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
