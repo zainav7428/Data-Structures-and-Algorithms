60816dc37d1bd2b60c95f1d6b91fdc1032c35ea7
zainav7428 <zainav7428@gmail.com>
2026-08-13 18:35:13 +0530

    Time: 2 ms (99.34%), Space: 47.3 MB (11.19%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int need = target - nums[i];
+
+            if(map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
