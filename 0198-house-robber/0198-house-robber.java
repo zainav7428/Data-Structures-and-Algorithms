9f0b6f5777b0da3c501e508fa0276941c2aba589
zainav7428 <zainav7428@gmail.com>
2026-08-18 21:37:29 +0530

    Time: 0 ms (100%), Space: 42.8 MB (38.42%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        if (n == 1) {
+        return nums[0];
+     }
+        
+        dp[0] = nums[0];
+     dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i =2;i<n;i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2] +nums[i]);
+        }
+        return dp[n-1];
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
