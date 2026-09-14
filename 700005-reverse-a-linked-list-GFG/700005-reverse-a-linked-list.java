037e9c0bda01b9b60e11c20a99d2f6f62d73599d
zainav7428 <zainav7428@gmail.com>
2026-09-15 00:27:42 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 700005-reverse-a-linked-list-GFG/700005-reverse-a-linked-list.java
@@ -0,0 +1,31 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        Node curr = head;
+        while(curr!=null){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
