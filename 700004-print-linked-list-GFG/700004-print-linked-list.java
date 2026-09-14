7a8c8e0a54c3ae7af9434933f4e57d4d44bc454b
zainav7428 <zainav7428@gmail.com>
2026-09-14 22:36:38 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 700004-print-linked-list-GFG/700004-print-linked-list.java
@@ -0,0 +1,28 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public ArrayList<Integer> printList(Node head) {
+         ArrayList<Integer> list = new ArrayList<>();
+        // code here
+        Node temp = head;
+        while(temp!=null){
+            list.add(temp.data);
+            
+            temp=temp.next;
+            
+        }
+        return list;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
